241d11f046133739b6d7b68f74935aafe4169d92
Dan Palmer <dan@danpalmer.me>
2016-01-16 16:01:31 +0000

    More tests

---
+++ tests/test_identities.py
@@ -0,0 +1,10 @@
+def test_empty_identity(assert_equal):
+    assert_equal('', '')
+
+def test_identity(assert_equal):
+    assert_equal('foo', 'foo')
+
+def test_identity_long(data_file, assert_equal):
+    input_content = data_file('simple_1.html')
+    expected = data_file('simple_1.out.html')
+    assert_equal(input_content, expected)
