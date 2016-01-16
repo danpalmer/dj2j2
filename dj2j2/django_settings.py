2a28d2e6e445a4a8ed1db7d02ad270b400ed4bbc
Dan Palmer <dan@danpalmer.me>
2016-01-16 14:27:50 +0000

    Add some Django setup and work for files/folders

---
+++ dj2j2/django_settings.py
@@ -0,0 +1,26 @@
+from django import setup
+from django.conf import settings
+
+
+def configure_django():
+    if configure_django._run:
+        return
+
+    settings.configure(
+        INSTALLED_APPS=[],
+        TEMPLATES=[
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [],
+                'APP_DIRS': True,
+                'OPTIONS': {
+                },
+            },
+        ],
+    )
+
+    setup()
+
+    configure_django._run = True
+
+configure_django._run = False
--- dj2j2/main.py
+++ dj2j2/main.py
@@ -4,17 +4,28 @@ import click
 from jinja2 import Template as JTemplate
 from django.template import Template as DTemplate
 
+from .django_settings import configure_django
+
 @click.command()
 @click.option('--indir', help="Input directory")
 @click.option('--outdir', help="Output directory")
-def run(indir, outdir):
+@click.option('--infile', help="Input file")
+@click.option('--outfile', help="Output file")
+def run(indir, outdir, infile, outfile):
     """
     A utility to transpile Django templates to Jinja2 templates.
     """
 
-    # Temporarily use files instead of directories
-    transpile(indir, outdir)
-    validate(outdir)
+    if infile and outfile:
+        transpile(infile, outfile)
+        validate(outfile)
+
+    elif indir and outdir:
+        pass
+
+    else:
+        # Invalid argument set
+        pass
 
 def transpile(infile_path, outfile_path):
     with open(infile_path, 'rb') as infile:
@@ -22,8 +33,9 @@ def transpile(infile_path, outfile_path):
             outfile.write(transpile_content(infile.read()))
 
 def transpile_content(incontent):
+    configure_django()
     template = DTemplate(incontent)
-    import ipdb; ipdb.set_trace()
+    return incontent
 
 def validate(outfile_path):
     try:
