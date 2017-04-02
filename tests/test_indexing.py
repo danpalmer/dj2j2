b16cf197090a878462b6560cfa2185fdd1b17b8e
Dan Palmer <dan@danpalmer.me>
2017-04-02 14:58:18 +0100

    Add a failing test for another thing need to fix

--- tests/test_indexing.py
+++ tests/test_indexing.py
@@ -3,3 +3,10 @@ def test_indexing(assert_equal):
         '{{ foo.0 }}',
         '{{ foo[0] }}',
     )
+
+
+def test_slicing(assert_equal):
+    assert_equal(
+        '{{ foo|slice:":3" }}',
+        '{{ foo[:3] }}',
+    )
